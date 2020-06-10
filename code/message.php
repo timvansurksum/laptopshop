16abff8dd415607dd24972cc4fc13e04a0fb4bcf
tim van surksum <325605@student.mboutrecht.nl>
2020-06-10 13:50:51 +0200

    create login form and checks on login script if both fields are entered

--- code/message.php
+++ code/message.php
@@ -9,7 +9,7 @@ $username = (isset($_GET["username"])) ? $_GET["username"] : "";
     // sets a default value for the refresh time and page for the redirection
 $time = 3;
 $page = "register";
-
+var_dump($_GET);
     // all the indevidual alerts
 switch ($_GET["alert"]) {
     case 'no-email':
@@ -71,6 +71,7 @@ switch ($_GET["alert"]) {
         you dont have rights on this page
         <hr>
         </div>';
+        exit;
         break;
     case 'passwords-unmatched';
         $time = 3;
