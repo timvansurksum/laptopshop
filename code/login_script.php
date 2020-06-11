e24c04fce9a23cf2dc240499780bc3195581e33a
tim van surksum <325605@student.mboutrecht.nl>
2020-06-11 10:51:49 +0200

    makes the comparision to the filed in password fully complete

--- code/login_script.php
+++ code/login_script.php
@@ -1,26 +1,27 @@
 <?php
 include("./classes.php");
 $register = new register;
-var_dump($_POST);
-echo "1";
-if (isset($_POST['username']) && isset($_POST['password'])) {
 
-echo "2";
+
+$username = $register->sanitize($_POST["username"]);
+$password = $register->sanitize($_POST["password"]);
+
+if (isset($username) && isset($password)) {
     
-    // Query password WHERE username = $_POST['username']
-    $sql = "SELECT `password` FROM `users` WHERE `username` = '{$_POST['username']}'";
+    // Query password WHERE username = $username
+    $sql = "SELECT `password` FROM `users` WHERE `username` = '{$username}'";
     $result = mysqli_query($register->conn, $sql);
+    // Password uit db vergelijken met form password    
     if (mysqli_num_rows($result)) {
-        if () {
-    // Password uit db vergelijken met form password
-    // zie password verify
-    // Als password klopt user = ingelogd     
+        if (password_verify($_POST["password"] , $result)) {
+    // Als password klopt user = ingelogd  
+    echo "succes";   
     }
     else {
     //anders error
     // header("location: ./index.php?content=message&alert=incorrect-password");
     }
-    echo 'success';
+    echo 'fail2';
     }
     else {
 echo "fail";
